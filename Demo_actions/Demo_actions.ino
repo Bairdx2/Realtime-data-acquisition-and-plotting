ddc78ed054f4e3996b47687e56fcd1719d2d01fc
Bairdx2 <pokeman1993@yahoo.com>
2018-08-22 14:57:59 -0400

    Update Demo_actions.ino

--- Demo_actions/Demo_actions.ino
+++ Demo_actions/Demo_actions.ino
@@ -5,7 +5,7 @@
 //        servo.                                              /
 //                                                            /
 // Authors:  Kevin Rivera                                     /
-// Creation Date:  12.04.18 v1.0                              /
+// Creation Date:  12.04.17 v1.0                              /
 ///////////////////////////////////////////////////////////////
 
 //Circuit diagrams:
